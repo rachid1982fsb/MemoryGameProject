593bd77671a3b5bcf23a325da415cd7718d97ef3
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-01-03 09:50:10 -0800

    Update UploadImages.js

--- src/components/UploadImages.js
+++ src/components/UploadImages.js
@@ -6,8 +6,8 @@ const config = {
     bucketName: 'memorycardgame',
     dirName: 'images', /* optional */
     region: 'us-west-2',
-    accessKeyId: '',
-    secretAccessKey: '',
+    accessKeyId: 'AKIAT4T7JSMVE2WQMIYD',
+    secretAccessKey: '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',
 }
 
 const UploadImages=(props)=>{
